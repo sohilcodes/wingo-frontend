45b6239a8327373ee88225ecd9b452dabd031e4f
Sohil Khan <pathansohilkhan2005@gmail.com>
2026-06-04 19:39:56 +0530

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -5,7 +6,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 };
